0338eed4e51a4275e7275d348901597f4119050e
Julián S <103692012+jusanchez6@users.noreply.github.com>
2023-11-13 12:39:57 -0500

    version 2

--- MAIN_BALANCIN/MAIN_BALANCIN.ino
+++ MAIN_BALANCIN/MAIN_BALANCIN.ino
@@ -18,7 +18,9 @@
 
 //Valor del pwm que saca de inercia a los motores. y donde
 // se almacenará la información de pwm que irá a cada motor
-#define PWM_I 100
+//y un factor qeue ayuda a regular los motores.
+
+#define PWM_I 95
 double PWM_A, PWM_B;
 
 //definición de las variables crudas donde se almacenará la lectua
@@ -35,15 +37,17 @@ MPU6050 sensor;
 
 //Definición de las variables de las constantes del controlador
 //PID. y de los parametros necesarios para el constructor del objeto tipo´PID
-//de la libreria PID_V1.h.También se inicia el objeto tipo PID.
+//de la libreria PID_V1.h.También se inicia el objeto tipo PID. Se declaran dos tipos
+//de variables de las constantes PID una agresiva para cuando el error sea muy grande
+//y un arreglo conservativo para cuando el error sea mucho más pequeño.
 
-double kp = 2.5; 
-double ki = 0.8;
-double kd = 600;
+
+double aggKp=2.8, aggKi=0.8, aggKd=20;
+double consKp=1, consKi=0, consKd=0;
 double Setpoint = 0;
 double PID_OUT;
 
-PID Controlador(&ang_x,&PID_OUT,&Setpoint,kp,ki,kd,DIRECT);
+PID Controlador(&ang_x, &PID_OUT, &Setpoint, aggKp, aggKi, aggKd, DIRECT);
 
 
 //Se define la función de reset por software:
@@ -67,6 +71,7 @@ void setup() {
   //alcanzar
 
   Setpoint = 0;
+  Controlador.SetSampleTime(50); 
 
   //se enciendo el PID
   Controlador.SetMode(AUTOMATIC);
@@ -89,11 +94,14 @@ void loop() {
   float accel_ang_x=atan(ay/sqrt(pow(ax,2) + pow(az,2)))*(180.0/3.14);
   
   //Calcular angulo de rotación con giroscopio y acelerometro
-  // y complemento con el filtro complemento, se le suma +8 al angulo
+  // y complemento con el filtro complemento, se le suma +15 al angulo
   // como correción para ubicar el angulo 0 donde queremos que se establezca. 
+
   ang_x = (0.98*(ang_x_prev+(gx/131)*dt) + 0.02*accel_ang_x);
   ang_x_prev=ang_x;
-  ang_x = ang_x+7,8;
+
+  ang_x = ang_x+15;
+
   if (isnan(ang_x)) {
       resetFunc();
     }
@@ -110,22 +118,36 @@ void loop() {
   
   float angle_error = Setpoint-ang_x_aux;
 
-  if(-3 < angle_error && angle_error < 3){
+  if(-5 < angle_error && angle_error < 5){
       ang_x = 0;
-      kp=0;
-      kd =0;
+      //Cambiamos los valores de las constantes pid para realizar un control
+      //mas conservativo, o mas suave. 
+      Controlador.SetTunings(consKp,consKi,consKd);
+      Controlador.Compute();
+
+      if(PID_OUT > 150){
+        resetFunc();
       }
 
+      }
 
-   Controlador.Compute();
+  //Si no estamos en el rango establecido por -3 y 3 entonces queremos volver a los valores de
+  // las constantes para un control mas agresivo.
 
+  else{
+    Controlador.SetTunings(aggKp,aggKi,aggKd);
+    Controlador.Compute();
+  }
+ 
 
   //Mapeamos los valores de pid para obtener el valor para enviarle
   // al motor. De 0 a 250 para el pid y la salida será de 0 a 150
   // debido a que ya tenemos un valor inicial que es de donde sale
   // los motores salen de la inercia.
-  
-  float PID_PWM = map(PID_OUT, 0, 250, 0, 150);
+  if(PID_OUT < 10){
+    PID_OUT = 10;
+  }
+  float PID_PWM = map(PID_OUT, 10, 250, 0, 150);
 
   //Analizamos mediante unos condicionales en que motor se necesita mas potencia. 
   // y los restamos o sumamos al valor donde el motor sale de la inercia. Lo hacemos
@@ -144,7 +166,7 @@ void loop() {
   //finalmente mandamos el valor analogico al puente H.
 
   analogWrite(enA, PWM_A);
-  analogWrite(enB, PWM_B*0.9);
+  analogWrite(enB, PWM_B*0.79);
 
   digitalWrite(inA_1, HIGH);
   digitalWrite(inA_2, LOW);
@@ -166,8 +188,8 @@ void loop() {
   Serial.print("  PWM_A: ");
   Serial.print(PWM_A);
   Serial.print("  PWM_B: ");
-  Serial.println(PWM_B*0.9);
+  Serial.println(PWM_B);
 
-  delay(20);
+  delay(50);
 
 }
